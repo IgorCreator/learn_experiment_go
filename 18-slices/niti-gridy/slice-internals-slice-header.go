efb179e8a63295d1390258816ceaa18d848022c7
Igor_Creator <asigoria@gmail.com>
2023-01-13 19:03:51 -0500

    Slices and backing arrays

--- 18-slices/niti-gridy/slice-internals-slice-header.go
+++ 18-slices/niti-gridy/slice-internals-slice-header.go
@@ -7,43 +7,46 @@ import (
 	s "github.com/inancgumus/prettyslice"
 )
 
-// type collection [4]string // #1
-type collection []string // #2
-
-// go is pass by copy
-// only the slice header is copied: 3 integer fields (24 bytes)
-// think of passing an array with millions of elements instead.
+type collection []string
 
 func main() {
 	// SliceHeader lives here:
 	// https://golang.org/src/runtime/slice.go
 
 	s.PrintElementAddr = true
-	data := collection{"slices", "are", "awesome", "period", "!!" /* #5 */}
+	data := collection{"slices", "are", "awesome", "period", "!!"}
 
-	// data := collection{"slices", "are", "awesome", "period", "!!"}
+	changeColl(data)
 
-	change(data) // #1
-
-	s.Show("main's data", data)                           // #1
-	fmt.Printf("main's data slice's header: %p\n", &data) // #3
+	s.Show("main's data", data)
+	fmt.Printf("main's data slice's header: %p\n", &data)
 
 	// ----------------------------------------------------------------------
 	// #4
-	array := [...]string{"slices", "are", "awesome", "period", "!!" /* #5 */}
+	array := [5]string{"Arrays", "are", "awesome", "period", "!!" /* #5 */}
+	changeArr(array)
+	s.Show("main's data", array)
 
 	// array's size depends on its elements
 	fmt.Printf("array's size: %d bytes.\n", unsafe.Sizeof(array))
-
 	// slice's size is always fixed: 24 bytes (on a 64-bit system) — slice value = slice header
 	fmt.Printf("slice's size: %d bytes.\n", unsafe.Sizeof(data))
 }
 
 // #1
 // passed value will be copied in the function
-func change(data collection) {
+func changeColl(data collection) {
 	// data is a new variable inside the function:
 	// var data collection
+
+	data[2] = "brilliant!"
+
+	s.Show("change's data", data)
+	fmt.Printf("change's data slice's header: %p\n", &data) // #3
+}
+
+func changeArr(data [5]string) {
+	// arrays do copies of originals
 
 	data[2] = "brilliant!"
 
