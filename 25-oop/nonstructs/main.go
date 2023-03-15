0b62e17e2b71f49f501864e719bbe792b1f17cc8
Igor_Creator <asigoria@gmail.com>
2023-03-15 18:42:25 -0400

    Clean up

--- 25-oop/nonstructs/main.go
+++ 25-oop/nonstructs/main.go
@@ -6,7 +6,6 @@ package main
 
 func main() {
 	var (
-		// mobydick  = book{title: "moby dick", price: 10}
 		minecraft = game{title: "minecraft", price: 20}
 		tetris    = game{title: "tetris", price: 5}
 	)
@@ -18,7 +17,6 @@ func main() {
 	// items -> []*game
 	// list  -> []*game
 	my := list(items)
-	// my = nil
 
 	// you can call methods even on a nil value
 	my.print()
